6e07240a1fa26a8089ede3fd50096773930d456c
nodbew <165175929+nodbew@users.noreply.github.com>
2024-04-18 23:00:56 +0900

    Update general.py

--- main/general.py
+++ main/general.py
@@ -5,6 +5,8 @@ Used to easen converting input to a correct format in streamlit_app.py
 '''
 from components.error import raise_error
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 def broadcast_add(df):
   '''
@@ -20,16 +22,21 @@ def broadcast_add(df):
     add(name,location)
   return
 
+def _name_location_dic_to_df():
+  dic = st.session_state._name_location_dictionary
+  names,locations = np.array([dic.keys()]),np.array([dic.values()])
+  return pd.DataFrame(np.stack([names,locations]).transpose(),columns=['名前','場所'])
+
 def filter(input): 
   '''
   Filters dataframe by whether input is contained in a key of st.session_state._name_location_dictionary
   '''
-  df = st.session_state._name_location_dictionary
+  df = _name_location_dic_to_df()
   return df[df['名前'].isin(input)]
 
 def edit(new_df):
   items = new_df[new_df['名前'] != None]
-  df = st.session_state._name_location_dictionary
+  df = _name_location_dic_to_df()
   for name,location in items:
     if name not in df:
       raise_error(f'{name}は登録されていません')
