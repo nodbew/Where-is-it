6e11e3b21f6b10ef65396d433eb2285eddfcb0bd
nodbew <165175929+nodbew@users.noreply.github.com>
2024-04-18 22:25:56 +0900

    Update broadcast.py

--- components/broadcast.py
+++ components/broadcast.py
@@ -6,5 +6,29 @@ Used to easen converting input to a correct format in streamlit_app.py
 import streamlit as st
 
 def broadcast_add(df):
+  '''
+  Base method is main.class.DataAdministor.add
+  '''
+  # Drop row with None since it might cause an error (Key already exists)
   indexes = (df['名前'] != None)
+  items = df[indexes]
 
+  # Add datas
+  add = st.session_state._DataAdmin.add
+  for name,location in items:
+    add(name,location)
+  return
+
+def filter(input): 
+  '''
+  Filters dataframe by whether input is contained in a key of st.session_state._name_location_dictionary
+  '''
+  df = st.session_state._name_location_dictionary
+  return df[df[column].isin(input)]
+
+def edit(new_df):
+  items = new_df[new_df['名前'] != None]
+  df = st.session_state._name_location_dictionary
+  for name,location in items:
+    name[df] = location
+  return
