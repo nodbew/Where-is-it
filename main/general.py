41cb706f8b389e93aa7edda4b10792a422fdaa3f
nodbew <165175929+nodbew@users.noreply.github.com>
2024-04-19 08:31:46 +0900

    Update general.py

--- main/general.py
+++ main/general.py
@@ -46,6 +46,8 @@ def filter(input,fullmatch):
     values = np.array(
       [st.session_state._name_location_dictionary[name] for name in names]
     )
+    st.write(names)
+    st.write(values)
 
     return pd.DataFrame(np.stack([names,values]).transpose(),
                         columns=['名前','場所'])
