7602fa3397542961287ba7ca7b50e9224c33e557
nodbew <165175929+nodbew@users.noreply.github.com>
2024-04-19 15:57:54 +0900

    Update dclass.py

--- main/dclass.py
+++ main/dclass.py
@@ -17,8 +17,7 @@ class DataAdminister:
         
       else:
         st.session_state._name_location_dictionary[name] = location
-        st.write(st.session_state._name_location_dictionary)
-        st.success(f'追加しました  debug info:args:{name},{location}')
+        st.success('追加しました')
         return
 
   def delete(self,name):
