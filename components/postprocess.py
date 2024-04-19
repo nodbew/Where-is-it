0f69195b80973d961f6df0b76bab0d150fb630b9
nodbew <165175929+nodbew@users.noreply.github.com>
2024-04-19 16:26:04 +0900

    Update postprocess.py

--- components/postprocess.py
+++ components/postprocess.py
@@ -1,10 +1,8 @@
 import streamlit as st
 
 def initialize():
-  st.success("successfully called init")
   if "_postprocess" not in st.session_state:
     st.session_state["_postprocess"] = dict()
-    st.success("defined")
   return
 
 def raise_error(message,execution=2):
@@ -42,6 +40,6 @@ def postprocess():
     exec(key)
 
     # Stop raising error when there is no execution count remaining
-    if st.session_state._errors[key] == 0:
-      del st.session_state._errors[key]
+    if st.session_state._postprocess[key] == 0:
+      del st.session_state._postprocess[key]
   return
