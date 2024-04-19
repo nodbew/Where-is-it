68a1aa3b6b5c05ae866c1c684a04bbe7380888a0
nodbew <165175929+nodbew@users.noreply.github.com>
2024-04-19 16:01:13 +0900

    Update general.py

--- main/general.py
+++ main/general.py
@@ -32,6 +32,7 @@ def filter(input,fullmatch):
   '''
   Filters dataframe by whether input is contained in a key of st.session_state._name_location_dictionary
   '''
+  st.success(f"debug info:{st.session_state._name_location_dictionary}")
   if fullmatch:
     try:
       return pd.DataFrame([[input,st.session_state._name_location_dictionary[input]]],
